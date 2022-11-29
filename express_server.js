df0bd162debf7e00b1a96f61c24d6b9404d78440
larsyaeger <larsyaeger@gmail.com>
2022-11-28 20:00:46 -0700

    added an app.use to use the urls_new template

--- express_server.js
+++ express_server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const app = express();
+const app = express()
 const PORT  = 8080;
 
 app.set('view engine', 'ejs');
@@ -8,7 +8,7 @@ const urlDatabase = {
   'b2xVn2': 'http://www.lighthouselabs.ca',
   '9sm5xK': 'http://www.google.com'
 };
-
+app.use(express.urlencoded({extended: true}));
 app.get('/', (req, res) => {
   res.send('hello');
   
@@ -24,10 +24,14 @@ app.get('/urls', (req, res) => {
   const templateVars = {urls: urlDatabase};
   res.render('urls_index', templateVars);
 });
+app.get('/urls/new', (req, res) => {
+  res.render('urls_new');
+});
 app.get('/urls/:id', (req, res) => {
   const templateVars = {id: req.params.id, longURL: urlDatabase[req.params.id]};
   res.render('urls_show', templateVars);
 });
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
